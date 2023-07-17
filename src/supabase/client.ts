ad0d0f619ca523042a98efe70b0aabfb2bd7f26d
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-07-17 16:44:15 +0530

    Supabase auth
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,4 +1,4 @@
-import { SupabaseClient } from "@supabase/supabase-js";
+import { SupabaseClient } from '@supabase/supabase-js'
 
 const client: SupabaseClient = new SupabaseClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -8,7 +8,7 @@ const client: SupabaseClient = new SupabaseClient(
       persistSession: true,
       autoRefreshToken: true,
     },
-  }
-);
+  },
+)
 
-export default client;
+export default client
